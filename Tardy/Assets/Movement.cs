8eeaaabac65dbb0108175fc141442b8ebee50f20
Lyzander Johnson <lyzanderjohnson@gmail.com>
2021-10-12 22:10:50 -0400

    Fixed the stupid jumping issues
    The player can now jump whenever they touch the ground

--- Tardy/Assets/Movement.cs
+++ Tardy/Assets/Movement.cs
@@ -4,36 +4,58 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-  /*Currently the forward movement, but 
+  /*Currently the forward movement works, but 
    * the player1 wont register when it touches the ground
    */
 
     public float baseSpeed = .00001f;
     public float speedMulti = 1.0f;
-  
+    public float jumpForce = 11000.0f;
     public bool isGrounded;
-    Rigidbody2D rb;
+    public Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.right * (baseSpeed * speedMulti);
     }
     
     // Update is called once per frame
     void Update()
     {
         
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            jump();
+        }
+        else
+        {
+            
+        }
     
-        rb.velocity = Vector2.right* (baseSpeed * speedMulti);
     }
-  private void OnTriggerEnter(Collider other)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (other.tag=="Ground")
+        if (collision.gameObject.tag == "Ground")
         {
             isGrounded = true;
-            Debug.Log("Triggered by Ground");
         }
     }
-   
+    void jump()
+    {
+
+       rb.velocity += Vector2.up * (jumpForce);
+        isGrounded = false;
+    }
+    /*
+      private void OnCollision2D(Collision2D other)
+      {
+          if (other.CompareTag("Ground"))
+          {
+              isGrounded = true;
+              Debug.Log("Triggered by Ground");
+          }
+      }*/
+
 }
