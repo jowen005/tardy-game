632016738ff513c8e5e18d149011486821509383
Lyzander Johnson <lyzanderjohnson@gmail.com>
2021-10-12 23:39:05 -0400

    Added Incremental Acceleration
    The player now increases velocity at set increments

--- Tardy/Assets/Movement.cs
+++ Tardy/Assets/Movement.cs
@@ -4,37 +4,42 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-  /*Currently the forward movement works, but 
-   * the player1 wont register when it touches the ground
-   */
 
+    //declaring variables
+    public float currentX=0f;
     public float baseSpeed = .00001f;
     public float speedMulti = 1.0f;
     public float jumpForce = 11000.0f;
+    public float speedBar = 100f;
     public bool isGrounded;
+    public float distMoved = 0f;
     public Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
-    {
+    {//declaring the rigidbody variable
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = Vector2.right * (baseSpeed * speedMulti);
+        //THIS is where to setup the initial speed, not in update
+     //   rb.velocity = Vector2.right * (baseSpeed * speedMulti);
+        
+        
     }
     
     // Update is called once per frame
     void Update()
     {
-        
+        float movement = baseSpeed * speedMulti;
+        transform.Translate(movement, 0.0f, 0.0f);
+
+        //testing if the player is on the ground and if the W key is pressed
         if (Input.GetKeyDown(KeyCode.W) && isGrounded)
         {
+         
             jump();
         }
-        else
-        {
-            
-        }
-    
+        multInc();
     }
+    //the function to detect if the player is touching the ground
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
@@ -42,20 +47,25 @@ public class Movement : MonoBehaviour
             isGrounded = true;
         }
     }
+    //the jumping function
     void jump()
     {
-
+        //adding to the overall velocity to jump
        rb.velocity += Vector2.up * (jumpForce);
         isGrounded = false;
     }
-    /*
-      private void OnCollision2D(Collision2D other)
-      {
-          if (other.CompareTag("Ground"))
-          {
-              isGrounded = true;
-              Debug.Log("Triggered by Ground");
-          }
-      }*/
-
+    
+    void multInc()
+    {
+        currentX = transform.position.x;
+       
+        distMoved = currentX - distMoved;
+        if (distMoved >= speedBar)
+        {
+            speedBar += 100f;
+            speedMulti += .01f;
+        }
+        currentX = transform.position.x;
+    }
+    
 }
